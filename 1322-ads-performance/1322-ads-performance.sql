d3421bb004ec12283ed7440fb5425b9660ee5daf
nitinroy3543 <36908483+nitinroy3543@users.noreply.github.com>
2022-06-27 09:03:48 -0700

    Time: 599 ms (51.38%), Space: 0B (100.00%) - LeetHub

---
+++ 1322-ads-performance/1322-ads-performance.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select ad_id, ifnull(round(sum(action = 'Clicked')/sum(action='Clicked' or action='Viewed')*100, 2) , 0) as ctr
+from Ads
+group by ad_id
+order by ctr desc, ad_id
