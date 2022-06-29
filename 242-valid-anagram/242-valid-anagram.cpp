658a65dce533742f90c5610b1f5fab78a7f57623
nitinroy3543 <36908483+nitinroy3543@users.noreply.github.com>
2022-06-29 15:54:17 -0700

    Time: 11 ms (69.43%), Space: 7.1 MB (94.58%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        
+        vector<int> table(26, 0);
+        for(char c: s) table[c-'a']++;
+        for(char c: t) {
+            table[c-'a']--;
+            if(table[c-'a']<0) return false;
+        }
+        
+        return true;
+    }
+};
