1322036e60227fe9c0c4c858e08d4dd1182d57c4
nitinroy3543 <36908483+nitinroy3543@users.noreply.github.com>
2022-07-05 17:23:07 -0700

    Time: 1277 ms (5.20%), Space: 0B (100.00%) - LeetHub

--- 1097-game-play-analysis-v/1097-game-play-analysis-v.sql
+++ 1097-game-play-analysis-v/1097-game-play-analysis-v.sql
@@ -4,6 +4,6 @@ select install_dt,count(distinct player_id) installs,
       from  (select player_id,event_date,
              min(event_date) over (partition by player_id order by event_date) as install_dt,
              lead(event_date) over (partition by player_id order by event_date) as next_dt
-            from activity) ba
+            from activity) as ba
 
         group by 1;
